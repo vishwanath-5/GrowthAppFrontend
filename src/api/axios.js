6682d664e739b97547a2435a13090c781c493450
vishwanath-5 <vishwanathnani1@gmail.com>
2026-04-02 18:52:03 +0530

    final api fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://growthappmiddleware.onrender.com",
+  baseURL: "https://growthappmiddleware.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
