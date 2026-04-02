4df32da4209b2360e28861bcb85a4d0a3cb9f270
vishwanath-5 <vishwanathnani1@gmail.com>
2026-04-02 17:52:34 +0530

    fixed finally

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: true,
   },
   preview: {
-    host: "0.0.0.0",
-    allowedHosts: "all", // ✅ easiest fix
+    host: true,
+    allowedHosts: ["growthappfrontend.onrender.com"],
   },
 });
