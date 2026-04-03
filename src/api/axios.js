b9a59f9a4c9d83b77124f1ca2f25b5254229507e
vishwanath-5 <vishwanathnani1@gmail.com>
2026-04-03 07:18:27 +0530

    will work

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://growthappmiddleware.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use((req) => {
